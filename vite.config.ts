6c63ae65838d60d90d9e63b6a1cfed0c9e1971f5
itskharina <137315976+itskharina@users.noreply.github.com>
2024-11-02 15:43:20 +0000

    Fix script path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react()],
-	base: '/javascript-github-profile/', // Replace with your repo name
+	base: '/javascript-github-profile/',
 });
